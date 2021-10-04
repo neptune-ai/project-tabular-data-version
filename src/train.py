86f1238126d9b7774ddeda5f65e8bddccafe6019
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2021-10-04 12:34:34 +0200

    files tracking added

--- src/train.py
+++ src/train.py
@@ -2,48 +2,57 @@ import pickle
 
 import neptune.new as neptune
 import numpy as np
+import pandas as pd
 import xgboost as xgb
 from neptune.new.integrations.xgboost import NeptuneCallback
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 base_namespace = "model_training"
 
-##########################
-# part 1: model training #
-##########################
+train_s3_path = "s3://kamil-projects/tabular-data/dataset-1/train/"
+valid_s3_path = "s3://kamil-projects/tabular-data/dataset-1/valid/"
+test_s3_path = "s3://kamil-projects/tabular-data/dataset-1/test/"
 
 # (neptune) create run
 run = neptune.init(
     project="common/project-tabular-data-version",
-    tags=["training"],
+    tags=["training", "S3"],
+    source_files=["train.py", "environment.yml"],
 )
 
-# (neptune-xgboost integration) create neptune_callback to track XGBoost training
-neptune_callback = NeptuneCallback(
-    run=run,
-    base_namespace=base_namespace,
-    log_tree=[0, 1, 2],
-)
+##########################
+# S3: track data version #
+##########################
 
-# prepare data
-X_train = ...
-y_train = ...
-X_valid = ...
-y_valid = ...
-X_test = ...
-y_test = ...
+# (neptune) track data version
+run["data/train"].track_files(train_s3_path)
+run["data/valid"].track_files(valid_s3_path)
+run["data/test"].track_files(test_s3_path)
+run.wait()
+
+# (neptune) prepare data
+run["data/train"].download(destination="train")
+run["data/valid"].download(destination="valid")
+run["data/test"].download(destination="test")
+run.wait()
+
+X_train = pd.read_csv("train/X.csv", index_col="Id")
+y_train = pd.read_csv("train/y.csv")
+X_valid = pd.read_csv("valid/X.csv", index_col="Id")
+y_valid = pd.read_csv("valid/y.csv")
+X_test = pd.read_csv("test/X.csv", index_col="Id")
+y_test = pd.read_csv("test/y.csv")
+
+# (neptune) log train sample
+run["data/train_sample"].upload(neptune.types.File.as_html(X_train.head(20)))
 
 dtrain = xgb.DMatrix(X_train, label=y_train)
 dval = xgb.DMatrix(X_valid, label=y_valid)
 dtest = xgb.DMatrix(X_test, label=y_test)
 
-# (neptune) track files
-run["data/train"].track_files("s3://kamil-projects/tabular-data/dataset-1/train/")
-run["data/valid"].track_files("s3://kamil-projects/tabular-data/dataset-1/valid/")
-run["data/test"].track_files("s3://kamil-projects/tabular-data/dataset-1/test/")
-
-# (neptune) log train sample
-run["data/train_sample"].upload(neptune.types.File.as_html(X_train.head(20)))
+###########################
+# XGBoost: model training #
+###########################
 
 # define parameters
 model_params = {
@@ -57,6 +66,13 @@ model_params = {
 }
 evals = [(dtrain, "train"), (dval, "valid")]
 num_round = 100
+
+# (neptune-xgboost integration) create neptune_callback to track XGBoost training
+neptune_callback = NeptuneCallback(
+    run=run,
+    base_namespace=base_namespace,
+    log_tree=[0, 1, 2],
+)
 
 # (neptune) pass neptune_callback to the train function and run training
 xgb.train(
